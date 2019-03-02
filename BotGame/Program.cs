6f1c65a09f246130845dd1c64a41fbe82ba8ec02
student <student@104-07>
2019-03-02 09:43:19 +0200

    Bot teleg

--- BotGame/Program.cs
+++ BotGame/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace BotGame
 {
     class Program
     {
+        static TelegramBotClient botClient;
+        
         static void Main(string[] args)
         {
+            try
+            {
+                botClient = new TelegramBotClient("765143820:AAFkn-60-4T-yf_ibmAhAsLvi8PnvJ3db_w");
+                var bot = botClient.GetMeAsync().Result;
+                Console.WriteLine(bot.Username);
+                
+                botClient.StartReceiving();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
         }
     }
 }
